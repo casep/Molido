4963cfa398485bc5ee5ce7c2534df95baa4b53f8
casep <carlos.sepulveda@gmail.com>
2014-05-21 10:51:14 -0400

    Agregado imapfilter

--- check_offlineimap.sh
+++ check_offlineimap.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 #Dumb script to check offlineimap and logging it
 
-echo "Inicio"   >  /tmp/offlineimap.log
-echo `date`     >> /tmp/offlineimap.log
-echo ""         >> /tmp/offlineimap.log
-offlineimap     >> /tmp/offlineimap.log
-echo ""         >> /tmp/offlineimap.log
-echo "Fin"      >> /tmp/offlineimap.log
-echo `date`     >> /tmp/offlineimap.log
-echo ""         >> /tmp/offlineimap.log
+echo "Inicio"      >  /tmp/offlineimap.log
+echo `date`        >> /tmp/offlineimap.log
+echo ""            >> /tmp/offlineimap.log
+echo "Imapfilter"  >> /tmp/offlineimap.log
+imapfilter -v      >> /tmp/offlineimap.log
+echo ""            >> /tmp/offlineimap.log
+echo "Offlineimap" >> /tmp/offlineimap.log
+offlineimap        >> /tmp/offlineimap.log
+echo ""            >> /tmp/offlineimap.log
+echo "Fin"         >> /tmp/offlineimap.log
+echo `date`        >> /tmp/offlineimap.log
+echo ""            >> /tmp/offlineimap.log
 
