b1763a8f987fe232587d030cd212fbfe70fb64d4
casep <carlos.sepulveda@gmail.com>
2016-04-04 17:27:47 +0100

    Later updates

--- fedoraPostInstall.sh
+++ fedoraPostInstall.sh
@@ -29,7 +29,7 @@
 sudo dnf install /home/casep/Utils/RPMs/i386/skype-4.3.0.37-fedora.i586.rpm
 sudo dnf install /home/casep/Utils/RPMs/x86_64/google-talkplugin_current_x86_64.rpm /home/casep/Utils/RPMs/x86_64/rpmfusion-free-release-23.noarch.rpm /home/casep/Utils/RPMs/x86_64/rpmfusion-nonfree-release-23.noarch.rpm /home/casep/Utils/RPMs/x86_64/google-chrome-stable_current_x86_64.rpm /home/casep/Utils/RPMs/x86_64/adobe-release-x86_64-1.0-1.noarch.rpm
 sudo rpm --import /home/casep/Utils/RPMs/x86_64/linux_signing_key.pub
-sudo dnf install totem rhythmbox lm_sensors wine.i686 nemo-dropbox evolution evolution-ews google-chrome-unstable google-chrome-stable gummi geany numpy remmina keepassx thinkfan git gcc gcc-c++ stress remmina-plugins-rdp remmina-plugins-vnc python2-matplotlib pwgen gstreamer1-libav gstreamer1-plugins-good gstreamer1-plugins-ugly gstreamer1-plugins-bad-free gstreamer-ffmpeg gstreamer-plugins-good gstreamer-plugins-ugly gstreamer-plugins-bad gstreamer-plugins-bad-free gstreamer-plugins-bad-nonfree dnf-automatic texmaker texlive-vmargin texlive-subfigure texlive-babel texlive-babel-english texlive-babel-spanish texlive-ulem autofs flash-plugin
+sudo dnf install totem rhythmbox lm_sensors wine.i686 nemo-dropbox evolution evolution-ews google-chrome-unstable google-chrome-stable gummi geany numpy remmina keepassx thinkfan git gcc gcc-c++ stress remmina-plugins-rdp remmina-plugins-vnc python2-matplotlib pwgen gstreamer1-libav gstreamer1-plugins-good gstreamer1-plugins-ugly gstreamer1-plugins-bad-free gstreamer-ffmpeg gstreamer-plugins-good gstreamer-plugins-ugly gstreamer-plugins-bad gstreamer-plugins-bad-free gstreamer-plugins-bad-nonfree dnf-automatic texmaker texlive-vmargin texlive-subfigure texlive-babel texlive-babel-english texlive-babel-spanish texlive-ulem autofs flash-plugin hunspell-es hunspell-en-GB aspell-en aspell-es VirtualBox kernel-devel
 sudo dnf remove thunderbird pidgin hexchat parole yumex yumex-dnf
 
 # Enable autofs
@@ -39,4 +39,6 @@ sudo sed -i '1i/home/casep/Hosted_Services     /etc/auto.cifs    --timeout=600 -
 # My hosts
 sudo rm -rf /etc/host ; sudo ln -s /home/casep/.ssh/hosts /etc/hosts
 
+#VirtualBox thingies
+sudo akmods; sudo systemctl restart systemd-modules-load.service
 exit 0
